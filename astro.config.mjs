cf9361ba6c7207ca62c186be691de8cddaac83e5
sejutaimpian <derissulistina@gmail.com>
2025-04-13 06:54:36 +0700

    Install Tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  integrations: [alpinejs()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
